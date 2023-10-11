18730eedf39db8f52c376c7bdcc2def54d67646b
Jeykop22 <nolanbinet2@gmail.com>
2023-10-11 09:46:02 +0200

    Write a C program that prints with printf(text)

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: '"C program for print a sentence"'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
