22c2b3b3ffe6a7f06d386904db4557452e292146
Jeykop22 <nolanbinet2@gmail.com>
2023-10-12 12:53:02 +0200

    100-intel generates the assembly code

---
+++ hello_world/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: '"C code and save it in an output file."'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	return(0);
+}
