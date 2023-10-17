6ec804411ef97591f9b9937227241405a28812b0
Jeykop22 <nolanbinet2@gmail.com>
2023-10-17 12:53:04 +0200

    3-islower.c check lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - make the sum of 1 number
+ * @c: First operand
+ *
+ * Return: the sum of the two parameters
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
