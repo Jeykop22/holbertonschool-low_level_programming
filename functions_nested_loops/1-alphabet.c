91b00133cb6af93f5cb8e2db82ff46256ee264de
Jeykop22 <nolanbinet2@gmail.com>
2023-10-17 11:00:22 +0200

    1-alphabet.c main. main.h

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print all alphabet in lower
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
