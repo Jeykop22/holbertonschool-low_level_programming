30a273ea5cf845351b9b230e48e8978bad218590
Jeykop22 <nolanbinet2@gmail.com>
2023-10-23 13:48:31 +0200

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,18 +6,22 @@
  */
 void puts_half(char *str)
 {
-	int i = 0;
 	int len = 0;
+	int i = 0;
 
 	while (str[len])
 	{
 		len++;
 	}
-
-	while (str[i] != '\0')
+	if (len % 2 == 0)
+		i = len / 2;
+	else
 	{
-		if (i >= len / 2)
-			_putchar(str[i]);
+		i = (len + 1) / 2;
+	}
+	while (str[i])
+	{
+		_putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');
