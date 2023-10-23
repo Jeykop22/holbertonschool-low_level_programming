8120c41f7df99b70e5e8e104063fdb3e874b331c
Jeykop22 <nolanbinet2@gmail.com>
2023-10-23 13:18:37 +0200

    6-puts2

---
+++ pointers_arrays_strings/6-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "bonjour comment va la vie de part chez toi";
+    puts2(str);
+    return (0);
+}
