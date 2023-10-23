3f687d9b63b8ec118fb65c545cd0cd7410f32dd3
Jeykop22 <nolanbinet2@gmail.com>
2023-10-23 12:16:17 +0200

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: pointer
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	for (len -= 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
