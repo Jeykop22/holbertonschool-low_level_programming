f4054e284c03dd6b9fa9592c687b361ee8af8178
Jeykop22 <nolanbinet2@gmail.com>
2023-10-23 15:18:34 +0200

    9-strcpy.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int res= 0;
+	int i = 0;
+
+	while (!((s[i] >= '0' && s[i] <= '9') || s[i] == '-' || s[i] == '+'))
+		i++;
+	while (s[i] && s[i] >= '0' && s[i] <= '9')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+			i++;
+		}
+		res = res * 10 + s[i] - '0';
+	}
+	return (0);
+}
