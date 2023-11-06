a9862e92d7122c80469e32250c8d445acad5e8c5
Jeykop22 <nolanbinet2@gmail.com>
2023-11-06 14:28:11 +0100

    4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,3 +1,18 @@
 #include "main.h"
+#include <stdlib.h>
 
-
+/**
+ * free_grid - function that frees a 2 dimensional grid
+ * @grid: ...
+ * @height: ... of the array
+ * Retrun: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
