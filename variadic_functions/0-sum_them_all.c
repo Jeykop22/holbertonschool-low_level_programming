a817ea1e4a65cb660bb90a92fe08622f2e5168b7
Jeykop22 <nolanbinet2@gmail.com>
2023-11-14 09:38:13 +0000

    0-sum_them_all.c

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+	int sum = 0;
+
+	if (n != 0)
+	{
+		va_start(args, n);
+		while (i < n)
+		{
+			sum += va_arg(args, int);
+			i++;
+		}
+		va_end(args);
+		return (sum);
+	}
+	return (0);
+}
