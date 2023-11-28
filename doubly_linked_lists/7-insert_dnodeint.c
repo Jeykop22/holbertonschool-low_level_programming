8ecd267e4a425a936e4b172153df8ff3663237c0
Jeykop22 <nolanbinet2@gmail.com>
2023-11-28 12:31:33 +0000

    fix value start of index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	else
 	{
 		head = *h;
-		i = 0;
+		i = 1;
 		if (head != NULL)
 			while (head->prev != NULL)
 				head = head->prev;
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			if (i == idx)
 			{
 				if (head->next == NULL)
-					head = add_dnodeint_end(h, n);
+					new_node = add_dnodeint_end(h, n);
 				else
 				{
 					new_node = malloc(sizeof(dlistint_t));
