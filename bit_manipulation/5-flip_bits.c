1a5ee9a467a5bd65539109b1d3d8c8ce757a741a
Jeykop22 <nolanbinet2@gmail.com>
2023-11-29 09:19:12 +0000

    5-flip_bits.c modified for loop

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -13,7 +13,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int index;
 
 	for (index = 0; n || m; n >>= 1, m >>= 1)
+	{
 		if ((n & 1) != (m & 1))
 			index++;
+	}
 	return (index);
 }
