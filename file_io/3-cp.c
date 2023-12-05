53b3c07ca09e5b16464a2d2aacc9060d7e74d386
Jeykop22 <nolanbinet2@gmail.com>
2023-12-05 11:32:12 +0000

    3-cp.c fix betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,20 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(int argc,char **argv)
+/**
+ * main - entry point
+ * @argc: numbers of arguments
+ * @argv: arguments
+ * Return: int
+*/
+int main(int argc, char **argv)
 {
 	int from_fd, to_fd, readed;
 	char *size[1024];
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
 	from_fd = open(argv[1], O_RDONLY);
 	if (!argv[1] || from_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit (98);
+		exit(98);
 	}
 	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((readed = read(from_fd, size, 1024)) > 0)
@@ -28,17 +35,17 @@ int main(int argc,char **argv)
 	if (readed == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
-		exit (98);
+		exit(98);
 	}
 	if (close(from_fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_fd);
-		exit (100);
+		exit(100);
 	}
 	if (close(to_fd) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_fd);
-		exit (100);
+		exit(100);
 	}
 	return (0);
 }
