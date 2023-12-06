18677316c47f7f05e3ac3b8db9f9b5cccf41b37c
Jeykop22 <nolanbinet2@gmail.com>
2023-12-06 12:59:08 +0000

    5-hash_table_print.c<fix betty

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+  * hash_table_print - Prints a hash table
+  * @ht: The hash table to prints
+  *
+  * Return: Nothing!
+  */
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node = NULL;
