c31a18dbb51ab525add386ffe705ad8d5346462c
Jeykop22 <nolanbinet2@gmail.com>
2024-01-17 12:11:30 +0000

    add folder search_algorithms, and all mandatory tasks

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef FILE_H
+#define FILE_H
+
+/* LIBRARIES */
+#include <stdio.h>
+#include <math.h>
+
+/* PROTOTYPES */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+#endif
